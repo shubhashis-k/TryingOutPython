535232ad5378180e3748242a4e8ba4a44b6cc852
shubhashis@gmail.com <shubhashis@gmail.com>
2015-06-19 11:35:55 -0700

    First commit- tried out asynchronous tornado

---
+++ main.py
@@ -0,0 +1,32 @@
+__author__ = 'shubhashis'
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.httpclient
+import urllib
+import json
+import datetime
+import time
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+class IndexHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        query = self.get_argument('q')
+        client = tornado.httpclient.AsyncHTTPClient()
+        response = client.fetch("http://localhost:8080/?q=lollipop", callback=self.my_func)
+
+
+    def my_func(self, response):
+        self.write(response.body)
+        self.finish()
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ syncTest.py
@@ -0,0 +1,27 @@
+__author__ = 'wildfire'
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.httpclient
+import urllib
+import json
+import datetime
+import time
+from tornado.options import define, options
+define("port", default=8888, help="run on the given port", type=int)
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        query = self.get_argument('q')
+        client = tornado.httpclient.HTTPClient()
+        response = client.fetch("http://localhost:8080/?q=asdasd")
+
+        self.write(response.body)
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
